7d2cc6d632af51b225d5550dff5d58ea36a12992
Andrey Colares Limberger <andreylimberger@hotmail.com>
2023-02-15 15:30:53 -0300

    Conversões, truthy, falsy

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+// tipo de dado
+// booleanos
+
+// conversão implícita
+const numero = 450;
+const numeroString = Number("450A");
+
+
+//Number()
+//String()
+
+console.log(numero + numeroString)
+
+
+// conversão explícita
---
+++ truthy-falsy.js
@@ -0,0 +1,14 @@
+
+/* console.log(0 == false)
+console.log("" == false)
+console.log(1 == true) */
+
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = "Andrey"
+
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
