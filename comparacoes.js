80b0ca12f3ddc11315979cf6d64177a48e7e1ebc
Andrey Colares Limberger <andreylimberger@hotmail.com>
2023-02-16 13:55:39 -0300

    Aula 04

---
+++ comparacoes.js
@@ -0,0 +1,14 @@
+// == (comparação implícita e explícita)
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero === texto)
+console.log(typeof numero)
+console.log(typeof texto)
+
+// == só compara o valor
+// === compara o valor e o tipo de dado
+
+Number()
+String()
---
+++ templates-strings.js
@@ -0,0 +1,10 @@
+const nome = "Andrey";
+const idade = 2023-1986;
+const cidadeDeNascimento = "Dom Feliciano."
+
+/* const apresetação = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci em " + cidadeDeNascimento;
+ */
+
+const apresetação = `Meu nome é ${nome}, minha idade é ${idade} e nasci em ${cidadeDeNascimento}`;
+
+console.log(apresetação)
