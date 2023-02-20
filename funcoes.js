e27ee97805047a9afe140737245d70b2d4e89bde
Andrey Colares Limberger <andreylimberger@hotmail.com>
2023-02-20 15:29:27 -0300

    Aula 05, parcial

---
+++ funcoes.js
@@ -0,0 +1,17 @@
+
+//Declaração de Função
+
+function imprimeTexto (texto) {
+    console.log(texto)
+}
+
+imprimeTexto ("Olá, mundo!")
+imprimeTexto ("Andrey")
+
+
+function soma () {
+    return 2+2;
+}
+
+console.log(soma())
+
---
+++ parametros.js
@@ -0,0 +1,18 @@
+ function soma (num1, num2) {
+    return num1+num2;
+}
+
+/* console.log (soma(45, 7))
+ */
+
+/* function nomeIdade (nome, idade) {
+    return `Meu nome é ${nome} e minha idade é ${idade}.`
+}
+
+console.log (nomeIdade("Andrey", 27)) */
+
+function multiplica (num1, num2) {
+    return num1 * num2;
+}
+
+console.log (multiplica(soma(4, 5), soma(3, 3)))
